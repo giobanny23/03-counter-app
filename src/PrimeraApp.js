e0d0941f942bc2669becdb5e14aa98eb0f1643f7
giobanny23 <edgar_2_62@hotmail.com>
2020-10-13 22:32:21 -0500

    wrapper

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types';
 //import React, {Fragment} from 'react';
 
 
-const PrimeraApp = ({saludo, subtitulo= 'Desarrolla Nuevas habilidades' }) => { 
+const PrimeraApp = ({saludo, subtitulo }) => { 
 
    
     return (
 
     <>
-        <h1> {saludo} </h1>
-    <p>{subtitulo}</p>
+        <h1> {saludo}!!! </h1>
+        <p>{subtitulo}</p>
     </>
     );
 }
 PrimeraApp.propsTypes = {
     saludo: PropTypes.string.isRequired
 }
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un Subtitulo'
+}
+
 export default PrimeraApp;
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -14,7 +14,24 @@ describe('Pruebas de PrimeraApp ', () => {
         const wrapper = shallow(<PrimeraApp  saludo={saludo}/>)
 
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe de mostrar el subtitulo enviado por Props', () => {
+
+        const saludo = 'Hola soy goku';
+        const subTitulo = 'Soy un  Subtitulo';
+
+        const wrapper = shallow(
+        <PrimeraApp  
+        saludo={subTitulo}
+        />
+        );
+
+        const textoParrafo = wrapper.find('p').text();
+        console.log(textoParrafo);
+        
     })
+    
     
     
     
